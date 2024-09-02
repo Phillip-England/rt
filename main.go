432f6d130e6bd349b871d5e9ebc7661297ad96b4
Phillip England <phillip.mark.england@gmail.com>
2024-09-02 16:27:25 -0500

    got file uploader working with attributes alone in the actual html

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/Phillip-England/vbf"
 )
 
+//=================================
+// COMPONENTS
+//=================================
+
 func IconBars() g.Component {
 	return g.Svg().Aria("hidden", "true").Xmlns("http://www.w3.org/2000/svg").Height("24").Width("24").Fill("none").ViewBox("0 0 24 24").In(
 		g.Path().Stroke("currentColor").StrokeLineCap("round").StrokeWidth("2").D("M5 7h14M5 12h14M5 17h14"),
@@ -37,6 +41,18 @@ func FormTitle(text string) g.Component {
 	return g.H2().Class("text-xl font-bold").Text(text)
 }
 
+//=================================
+// SHARED BEHAVIOR
+//=================================
+
+func ToggleNav(c g.Component) g.Component {
+	return c.Attr("ht-mass-toggle", "click:#header-bars #header-x #nav-overlay #nav-menu:hidden")
+}
+
+//=================================
+// LAYOUTS
+//=================================
+
 func Layout(isOpen bool) g.Component {
 	return g.HTMLDoc().In(
 		g.Head().In(
@@ -44,7 +60,6 @@ func Layout(isOpen bool) g.Component {
 			g.Link().Rel("stylesheet").Href("/static/css/output.css"),
 			g.Script().Src("/static/js/rat.js"),
 			g.Script().Src("/static/js/htpl.js"),
-			g.Script().Src("https://unpkg.com/htmx.org@2.0.2"),
 			g.Title().Text("Receipt Tracker"),
 		),
 		g.Body().Attr("hx-boost", "true").In(
@@ -54,48 +69,35 @@ func Layout(isOpen bool) g.Component {
 						g.H1().Class("text-xl font-bold").Text("Receipt Tracker"),
 						g.P().Class("text-sm").Text("Where's my money?"),
 					),
-					g.Div().Class("flex items-center").In(
-						g.IfElse(isOpen,
-							g.A().Href("/").In(
-								IconX(),
-							),
-							g.A().Href(`/?open=true`).In(
-								IconBars(),
-							),
-						),
+					g.Div().Of(ToggleNav).Class("flex items-center cursor-pointer").In(
+						IconBars().ID("header-bars"),
+						IconX().ID("header-x").Class("hidden"),
 					),
 				),
 				g.Main().Class("flex flex-col items-center md:p-12").In(
-					g.Form().Attr("action", "/").Attr("scan", "#upload-button #hidden-button").ID("receipt-form").Class("rounded flex flex-col w-full gap-8 p-4 max-w-[500px]").In(
+					g.Form().Attr("ht-multi-photo-form", "#hidden-file-input:#photo-container:flex:0.5").Attr("action", "/").Attr("scan", "#upload-button #hidden-button").ID("receipt-form").Class("rounded flex flex-col w-full gap-8 p-4 max-w-[500px]").In(
 						FormTitle("Upload Receipts"),
 						FormTextInput("Name", "name"),
 						FormTextArea("What are these expenses for?", "reason"),
 						g.Input().Attr("ht-click-proxy", "#hidden-file-input").Type("button").ID("upload-button").Class("bg-blue-700 text-white rounded w-fit text-sm py-1 px-4").Value("select file"),
-						g.Input().Type("file").Class("hidden").ID("hidden-file-input").Name("file"),
+						g.Input().Type("file").Class("hidden").ID("hidden-file-input").Name("file").Attr("multiple", ""),
 						g.Button().Class("bg-black py-2 px-4 rounded text-white text-sm").Text("Submit"),
 					),
-					g.Div().Class("flex flex-wrap gap-8 p-4 w-full").ID("photo-container"),
+					g.Div().Class("flex flex-col gap-8 p-4 w-full").ID("photo-container"),
 				),
-				g.If(isOpen,
-					g.A().Href("/").In(
-						g.Div().Class("absolute top-0 h-full w-full bg-black z-30 opacity-50"),
-					),
-				),
-				g.If(isOpen,
-					g.Nav().Class("absolute w-4/5 border-r h-full z-30 bg-white").In(
-						g.Div().Class("h-[85px]"),
-						g.Ul().Class("flex flex-col gap-2 p-2 w-full text-sm").In(
-							g.Li().Class("flex").In(
-								g.A().Class("p-4 w-full border rounded").Href("/").Text("Home"),
-							),
-							g.Li().Class("flex").In(
-								g.A().Class("p-4 w-full border rounded").Href("/about").Text("About"),
-							),
+				g.Div().Of(ToggleNav).ID("nav-overlay").Class("absolute top-0 h-full w-full bg-black z-30 opacity-50 hidden"),
+				g.Nav().ID("nav-menu").Class("absolute w-4/5 border-r h-full z-30 bg-white hidden").In(
+					g.Div().Class("h-[85px]"),
+					g.Ul().Class("flex flex-col gap-2 p-2 w-full text-sm").In(
+						g.Li().Class("flex").In(
+							g.A().Class("p-4 w-full border rounded").Href("/").Text("Home"),
+						),
+						g.Li().Class("flex").In(
+							g.A().Class("p-4 w-full border rounded").Href("/about").Text("About"),
 						),
 					),
 				),
 			),
-			g.Script().Text(`htpl.hook()`),
 		),
 	)
 }
--- static/js/htpl.js
+++ static/js/htpl.js
@@ -2,11 +2,16 @@ class Htpl {
 
     constructor() {
         this.attributes = {}
+        this.ctx = {}
     }
 
     
     add(attribute, fn) {
         this.attributes[attribute] = fn
+    }
+
+    stamp(element) {
+        element.setAttribute('ht-stamp', 'true')
     }
 
     hook() {
@@ -29,8 +34,6 @@ class Htpl {
     }
 }
 
-
-
 const htpl = new Htpl()
 
 htpl.add("ht-click-proxy", (element, attr) => {
@@ -38,4 +41,173 @@ htpl.add("ht-click-proxy", (element, attr) => {
     element.addEventListener('click', () => {
         target.click()
     })
+})
+
+htpl.add('ht-mass-toggle', (element, attr) => {
+    let parts = attr.split(":")
+    let eventType = parts[0]
+    let targets = parts[1].split(' ')
+    let classes = parts[2].split(' ')
+    element.addEventListener(eventType, () => {
+        for (let i = 0; i < targets.length; i++) {
+            let target = document.querySelector(targets[i])
+            for (let i2 = 0; i2 < classes.length; i2++) {
+                let cls = classes[i2]
+                target.classList.toggle(cls)
+            }
+        }
+    })
+})
+
+htpl.add('ht-multi-photo-form', (element, attr) => {
+
+    function extract() {
+        let parts = attr.split(':')
+        if (parts.length < 2) {
+            console.error('ht-multi-photo-form does not have enough args')
+        }
+        let fileInputSelector = parts[0]
+        let photoContainerSelector = parts[1]
+        let canvasWrapperClasses = ""
+        if (parts[2]) {
+            canvasWrapperClasses = parts[2].split(" ")
+        }
+        let scaleRatio = 1
+        if (parts[3]) {
+            scaleRatio = Number(parts[3])
+        }
+        let fileInput = document.querySelector(fileInputSelector)
+        let photoContainer = document.querySelector(photoContainerSelector)
+        let data = new DataTransfer()
+        return {fileInput, photoContainer, scaleRatio, canvasWrapperClasses, data}
+    }
+
+    function getDiv(file, canvasWrapperClasses) {
+        let div = document.createElement('div')
+        div.setAttribute('key', file.name)
+        for (let i = 0; i < canvasWrapperClasses.length; i++) {
+            let cls = canvasWrapperClasses[i]
+            div.classList.add(cls)
+        }
+        return div
+    }
+
+    function getCanvas(scaleRatio, img) {
+        let canvas = document.createElement('canvas')
+        canvas.height = img.height * scaleRatio
+        canvas.width = img.width * scaleRatio
+        return {
+            'canvas': canvas,
+            'ctx': canvas.getContext('2d')
+        }
+    }
+
+    function draw(canvas, div, photoContainer, ctx, img) {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+        div.appendChild(canvas)
+        photoContainer.appendChild(div)
+    }
+
+    let {fileInput, photoContainer, scaleRatio, canvasWrapperClasses, data} = extract()
+
+    fileInput.addEventListener('change', (e) => {
+        e.preventDefault()
+        let file = fileInput.files[0]
+        if (!file) {
+            return
+        }
+        let reader = new FileReader()
+        if (!reader) {
+            return
+        }
+        reader.onload = function(event) {
+            let img = new Image()
+            if (!img) {
+                return
+            }
+            img.onload = function() {
+                data.items.add(file)
+                fileInput.files = data.files
+                let div = getDiv(file, canvasWrapperClasses)
+                let { canvas, ctx } = getCanvas(scaleRatio, img)
+                draw(canvas, div, photoContainer, ctx, img)
+                div.addEventListener('click', () => {
+                    let newFiles = new DataTransfer()
+                    for (let i = 0; i < fileInput.files.length; i++) {
+                        let f = fileInput.files[i]
+                        let found = false
+                        if (f.name == div.getAttribute('key') && found == false) {
+                            found = true
+                            div.remove()
+                            continue
+                        }
+                        newFiles.items.add(f)
+                    }
+                    fileInput.files = newFiles.files
+                })
+            }
+            img.src = event.target.result
+        }
+        reader.readAsDataURL(file)
+    })
+
+
+
+    // let selectedFiles = []
+    // fileInput.addEventListener('change', (e) => {
+    //     e.preventDefault()
+    //     let file = fileInput.files[0]
+    //     selectedFiles.push({
+    //         "key": selectedFiles.length,
+    //         "file": file
+    //     })
+    //     let canvas = document.createElement('canvas')
+    //     let div = document.createElement('div')
+    //     div.setAttribute('key', selectedFiles.length-1)
+    //     for (let i = 0; i < canvasWrapperClasses.length; i++) {
+    //         let cls = canvasWrapperClasses[i]
+    //         div.classList.add(cls)
+    //     }
+    //     div.addEventListener('click', () => {
+    //         div.remove()
+    //         let dataTransfer = new DataTransfer()
+    //         for (let i = 0; i < selectedFiles.length; i++) {
+    //             let f = selectedFiles[i]
+    //             let key = f.key
+    //             let file = f.file
+    //             let divKey = div.getAttribute('key')
+    //             console.log(key, divKey)
+    //             if (key == divKey) {
+    //                 selectedFiles.splice(i, 1)
+    //                 break
+    //             }
+    //             dataTransfer.items.add(file)
+    //         }
+    //         fileInput.files = dataTransfer.files
+    //     })
+    //     let reader = new FileReader()
+    //     reader.onload = function(event) {
+    //         let img = new Image()
+    //         img.onload = function() {
+    //             canvas.height = img.height * scaleRatio
+    //             canvas.width = img.width * scaleRatio
+    //             let ctx = canvas.getContext('2d')
+    //             ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+    //             div.appendChild(canvas)
+    //             photoContainer.appendChild(div)
+    //         }
+    //         img.src = event.target.result
+    //     }
+    //     reader.readAsDataURL(file)
+    //     let dataTransfer = new DataTransfer()
+    //     for (let i = 0; i < selectedFiles.length; i++) {
+    //         let f = selectedFiles[i].file
+    //         dataTransfer.items.add(file)
+    //     }
+    //     fileInput.files = dataTransfer.files
+    // })
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    htpl.hook()
 })
